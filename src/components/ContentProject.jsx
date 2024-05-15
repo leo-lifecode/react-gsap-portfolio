25a4e92c5180bfc38bd911096faecc224a6419de
Fahryzal <fahrjilo@gmail.com>
2024-05-15 21:39:13 +0700

    fix project section

--- src/components/ContentProject.jsx
+++ src/components/ContentProject.jsx
@@ -1,27 +1,53 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const ContentProject = ({ img, link, name, brand, children, stylecustom }) => {
+const ContentProject = ({
+  img,
+  link,
+  name,
+  brand,
+  children,
+  stylecustom,
+  id,
+  text,
+}) => {
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(() => {
+    gsap.from(`#${text}`, {
+      scrollTrigger: {
+        trigger: `#${id}`,
+        start: "-=380",
+      },
+      stagger: 0.023,
+      duration: 0.5,
+      y: 200,
+    });
+  });
   return (
-    <div className="grid grid-cols-12 items-center gap-[12px] overflow-hidden py-[100px] font-neue lg:gap-[20px]">
+    <div
+      id={id}
+      className=" grid grid-cols-12 items-center gap-[12px] overflow-hidden py-[100px] font-neue lg:gap-[20px]"
+    >
       <div className="col-span-12 lg:col-span-5">
-        <div className="text-[42px] font-semibold capitalize sm:text-[68px] lg:text-[58px] xl:text-[72px]">
-          {name}
+        <div className="overflow-hidden text-[42px] font-semibold capitalize sm:text-[68px] lg:text-[58px] xl:text-[72px]">
+          <p id={text}>{name}</p>
         </div>
-        <a
-          href={link}
-          target="_blank"
-          className="text-[14px] text-gray-500 sm:text-[18px]"
-        >
-          {link}
-        </a>
-        <div className="my-[4px] flex gap-x-1 text-[14px] font-medium sm:my-[12px] sm:text-[28px] lg:text-[24px] xl:gap-x-[12px] xl:text-[32px]">
-          <div>{brand}</div>
-          <div>•</div>
-          <div>Front-end developer</div>
+        <div className="overflow-hidden text-[14px] text-gray-500 sm:text-[18px]">
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <div href={link} target="_blank" id={text}>
+              {link}
+            </div>
+          </a>
+        </div>
+        <div className=" my-[4px] flex gap-x-1 overflow-hidden text-[14px] font-medium sm:my-[12px] sm:text-[28px] lg:text-[24px] xl:gap-x-[12px] xl:text-[32px]">
+          <div id={text}>{brand}</div>
+          <div id={text}>•</div>
+          <div id={text}>Front-end developer</div>
         </div>
         <div>
-          <div className="text-[12px] sm:text-xl lg:text-[20px] xl:text-2xl">
-            {children}
+          <div className="overflow-hidden text-[12px] sm:text-xl lg:text-[20px] xl:text-2xl">
+            <p id={text}>{children}</p>
           </div>
         </div>
       </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -33,11 +33,28 @@ const Project = () => {
       },
       "+=1.5",
     );
+    gsap.to("#section_1", {
+      scrollTrigger: {
+        trigger: "#lastProject",
+        start: "top",
+        scrub: 0.5,
+      },
+      scale: 0.95,
+    });
+
+    gsap.from(`#textfive`, {
+      scrollTrigger: {
+        trigger: `#lastProject`,
+        start: "-=340",
+      },
+      stagger: 0.023,
+      y: 200,
+    });
   });
   return (
     <div
       id="section_1"
-      className="relative z-[99] flex rounded-t-3xl bg-black px-[1.5rem] py-[3.5rem] sm:px-[4%] sm:py-[5%]"
+      className="relative z-[99] flex rounded-3xl bg-black px-[1.5rem] py-[3.5rem] sm:px-[4%] sm:py-[5%]"
     >
       <div className="w-full text-[#d1d1c7]">
         <div className="overflow-hidden font-neue text-[38px] font-bold uppercase leading-[1.2] sm:text-[84px] lg:text-[96px]">
@@ -46,6 +63,8 @@ const Project = () => {
           </div>
         </div>
         <ContentProject
+          text="projecttextone"
+          id="projectone"
           link="https://shoply-tnbcommerce.vercel.app/"
           img={project1}
           brand="E-Commerce"
@@ -55,41 +74,82 @@ const Project = () => {
           Multi brand e-commerce design system for websites and mobile.
         </ContentProject>
         <ContentProject
+          text="projecttexttwo"
+          id="projecttwo"
           link="https://shoply-tnbcommerce.vercel.app/"
           img={project2}
           brand="Music"
           name="Musicly"
           stylecustom=""
         >
-          Multi brand e-commerce design system for websites and mobile.
+          music products with features like responsive design, intuitive
+          navigation, and efficient product categorization
         </ContentProject>
         <ContentProject
+          text="projecttextthree"
+          id="projectthree"
           link="https://weather-app-reactweathr.vercel.app/"
           img={project32}
           brand="Weather"
           name="Weather app"
           stylecustom=""
         >
-          Multi brand e-commerce design system for websites and mobile.
+          The Weather App is a web application that delivers real-time weather
+          updates.
         </ContentProject>
         <ContentProject
+          text="projecttextfour"
+          id="projectfour"
           link="https://todo-list-reactapp-three.vercel.app/"
           img={project4}
           brand="Tasks"
           name="Todo List"
           stylecustom=""
         >
-          Multi brand e-commerce design system for websites and mobile.
+          The Todo List project is a responsive React web app for managing
+          tasks, featuring task categorization, due dates, and prioritization.
         </ContentProject>
-        <ContentProject
-          link="https://nfthouse.vercel.app/"
-          img={project5}
-          brand="Cyrypto"
-          name="NFT HOUSE"
-          stylecustom=""
+        <div
+          id="lastProject"
+          className="grid grid-cols-12 items-center gap-[12px] overflow-hidden py-[100px] font-neue lg:gap-[20px]"
         >
-          Multi brand e-commerce design system for websites and mobile.
-        </ContentProject>
+          <div className="col-span-12 lg:col-span-5">
+            <div className="overflow-hidden text-[42px] font-semibold capitalize sm:text-[68px] lg:text-[58px] xl:text-[72px]">
+              <p id="textfive">NFT HOUSE</p>
+            </div>
+            <div className="overflow-hidden text-[14px] text-gray-500 sm:text-[18px]">
+              <a
+                href="https://nft-house.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div id="textfive">https://nft-house.vercel.app</div>
+              </a>
+            </div>
+            <div className=" my-[4px] flex gap-x-1 overflow-hidden text-[14px] font-medium sm:my-[12px] sm:text-[28px] lg:text-[24px] xl:gap-x-[12px] xl:text-[32px]">
+              <div id="textfive">NFT</div>
+              <div id="textfive">•</div>
+              <div id="textfive">Front-end developer</div>
+            </div>
+            <div>
+              <div className="overflow-hidden text-[12px] sm:text-xl lg:text-[20px] xl:text-2xl">
+                <p id="textfive">
+                  NFT HOUSE is a responsive web slicing figma to HTML CSS and
+                  JAVASCRIPT
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-12 me-[-50%]  overflow-hidden rounded-2xl border-2 border-white lg:col-span-7 lg:me-[-35%] xl:me-[-50%]">
+            <a
+              href="https://nfthouse.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={project5} className="" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
